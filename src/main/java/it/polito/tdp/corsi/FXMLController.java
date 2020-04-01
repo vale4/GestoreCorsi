0b9ed4123512f8599c277d27ef49ce779fdb3719
albertomonge <alberto.monge@polito.it>
2020-04-01 15:28:06 +0200

    soluzione seconda parte

--- src/main/java/it/polito/tdp/corsi/FXMLController.java
+++ src/main/java/it/polito/tdp/corsi/FXMLController.java
@@ -11,6 +11,7 @@ import java.util.ResourceBundle;
 
 import it.polito.tdp.corsi.model.Corso;
 import it.polito.tdp.corsi.model.Model;
+import it.polito.tdp.corsi.model.Studente;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -105,12 +106,54 @@ public class FXMLController {
 
     @FXML
     void stampaDivisione(ActionEvent event) {
-
+    	
+    	txtRisultato.clear();
+    	
+    	String codins = txtCorso.getText();
+    	
+    	//controllo se il codice corrisponde a un corso esistente
+    	if(!this.model.esisteCorso(codins)) {
+    		txtRisultato.appendText("Il corso non esiste!\n");
+    		return ;
+    	}
+    	
+    	//OUTPUT dato un corso, ci aspettiamo una divisione del genere 
+    	// Informatica 12
+    	// Gestionali 28
+    	
+    	Map<String, Integer> statistiche = this.model.getDivisioneCDS(new Corso(codins, null, null, null));
+    	
+    	for(String cds : statistiche.keySet()) {
+    		txtRisultato.appendText(cds + " " + statistiche.get(cds) + "\n");
+    	}
+    	
     }
 
     @FXML
     void stampaStudenti(ActionEvent event) {
-
+    	txtRisultato.clear();
+    	
+    	String codins = txtCorso.getText();
+    	
+    	//controllo se il codice corrisponde a un corso esistente
+    	if(!this.model.esisteCorso(codins)) {
+    		txtRisultato.appendText("Il corso non esiste!\n");
+    		return ;
+    	}
+    	
+    	
+    	List<Studente> studenti = this.model.getStudentiByCorso(new Corso(codins, null,null,null));
+    	
+    	if(studenti.size() == 0) {
+    		txtRisultato.appendText("Il corso non ha studenti iscritti");
+    		return;
+    	} 
+    	
+    	for(Studente s : studenti) {
+    		txtRisultato.appendText(s.toString() + "\n");
+    	}
+    	
+    	
     }
 
     @FXML // This method is called by the FXMLLoader when initialization is complete
--- src/main/java/it/polito/tdp/corsi/model/Model.java
+++ src/main/java/it/polito/tdp/corsi/model/Model.java
@@ -1,5 +1,6 @@
 package it.polito.tdp.corsi.model;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -13,6 +14,10 @@ public class Model {
 		dao = new CorsoDAO();
 	}
 	
+	public boolean esisteCorso(String codins) {
+		return dao.esisteCorso(codins);
+	}
+	
 	public List<Corso> getCorsiByPeriodo(Integer pd){
 		return dao.getCorsiByPeriodo(pd);
 	}
@@ -21,4 +26,32 @@ public class Model {
 	public Map<Corso,Integer> getIscrittiByPeriodo(Integer pd){
 		return dao.getIscrittiByPeriodo(pd);
 	}
+	
+	public List<Studente> getStudentiByCorso(Corso c){
+		return dao.getStudentiByCorso(c);
+	}
+	
+	
+	public Map<String, Integer> getDivisioneCDS(Corso c){
+		//SOLUZIONE 1
+		/*List<Studente> studenti = dao.getStudentiByCorso(c);
+		
+		Map<String, Integer> statistiche = new HashMap<String, Integer>();
+		
+		for(Studente s : studenti) {
+			
+			if(s.getCDS() != null && !s.getCDS().equals("")) {
+				if(statistiche.containsKey(s.getCDS())) {
+					statistiche.put(s.getCDS(),statistiche.get(s.getCDS()) +1);
+				} else {
+					statistiche.put(s.getCDS(), 1);
+				}		
+			}
+		}
+		
+		return statistiche;*/
+		return dao.getDivisioneCDS(c);
+	}
+	
+	
 }
