e9d73a17d6a2f51f1b74b271777a22bad3736ce7
Maco <Maco@LAPTOP-O1JLL8OB>
2020-04-13 21:32:08 +0200

    Completato - Parte 2

--- src/main/java/it/polito/tdp/corsi/db/ConnectDB.java
+++ src/main/java/it/polito/tdp/corsi/db/ConnectDB.java
@@ -1,6 +1,7 @@
 package it.polito.tdp.corsi.db;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 
 import com.zaxxer.hikari.HikariConfig;
@@ -8,7 +9,12 @@ import com.zaxxer.hikari.HikariDataSource;
 
 public class ConnectDB {
 	
-	private static final String jdbcURL="jdbc:mysql://localhost/iscritticorsi";
+	public static Connection getConnection() throws SQLException{
+		String jdbcURL="jdbc:mysql://localhost/iscritticorsi?user=root&password=valentina";
+		return DriverManager.getConnection(jdbcURL);
+	}
+	
+	/*private static final String jdbcURL="jdbc:mysql://localhost/iscritticorsi";
 	private static HikariDataSource ds;
 	
 	public static Connection getConnection() {
@@ -33,6 +39,6 @@ public class ConnectDB {
 			throw new RuntimeException(e);
 		}
 		
-	}
+	}*/
 
 }
--- src/main/java/it/polito/tdp/corsi/db/CorsoDAO.java
+++ src/main/java/it/polito/tdp/corsi/db/CorsoDAO.java
@@ -6,12 +6,34 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
 import it.polito.tdp.corsi.model.Corso;
+import it.polito.tdp.corsi.model.Studente;
 
 public class CorsoDAO {
+	
+	public boolean esisteCorso(String codins) {
+		String sql="SELECT * FROM corso WHERE codins=? ";
+		
+		try {
+			Connection conn = ConnectDB.getConnection();
+			PreparedStatement st=conn.prepareStatement(sql);
+			st.setString(1, codins);
+			ResultSet rs=st.executeQuery();
+			
+			if(rs.next()) {
+				conn.close();
+				return true;
+			}else
+				conn.close();
+				return false;
+		}catch(SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
 	
 	public List<Corso>getCorsiByPeriodo(Integer pd){
 		String sql="SELECT * FROM corso WHERE pd= ?";
@@ -62,6 +84,55 @@ public class CorsoDAO {
 			throw new RuntimeException(e);
 		}
 		return result;
+	}
+	
+	
+	public List<Studente>getStudentiByCorso(Corso corso){
+		String sql="SELECT s.matricola, s.nome, s.cognome, s.CDS " + 
+				"FROM studente AS s, iscrizione AS i " + 
+				"WHERE s.matricola=i.matricola AND i.codins=? ";
+		List<Studente>studenti=new LinkedList<Studente>();
+		
+		try {
+			Connection conn=ConnectDB.getConnection();
+			PreparedStatement st=conn.prepareStatement(sql);
+			st.setString(1, corso.getCodins());
+			ResultSet rs=st.executeQuery();
+			
+			while(rs.next()) {
+				Studente s=new Studente(rs.getInt("matricola"), rs.getString("nome"), rs.getString("cognome"), rs.getString("CDS"));
+				studenti.add(s);
+			}
+			conn.close();
+			
+		}catch(SQLException e) {
+			throw new RuntimeException(e);
+		}
+		
+		return studenti;
+		
+	}
+	
+	public Map<String, Integer>getDivisioneCDS(Corso c){
+		String sql="SELECT s.CDS, COUNT(*) AS tot " + 
+				"FROM studente AS s, iscrizione AS i " + 
+				"WHERE s.matricola=i.matricola AND s.CDS<>\"\" AND i.codins=? " + 
+				"GROUP BY s.CDS ";
+		Map<String, Integer>statistiche=new HashMap<String, Integer>();
+		try {
+			Connection conn=ConnectDB.getConnection();
+			PreparedStatement st=conn.prepareStatement(sql);
+			st.setString(1, c.getCodins());
+			ResultSet rs=st.executeQuery();
+			
+			while(rs.next()) {
+				statistiche.put(rs.getString("CDS"), rs.getInt("tot"));
+			}
+			conn.close();
+		}catch(SQLException e) {
+			throw new RuntimeException(e);
+		}
+		return statistiche;
 	}
 
 }
